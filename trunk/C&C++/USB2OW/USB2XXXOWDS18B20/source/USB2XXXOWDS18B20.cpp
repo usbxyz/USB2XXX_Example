fab86c94fe0d7c5f01c4eff9353ac7d4b854a82f
creators <creators@sina.cn>
2017-07-10 16:56:27 +0800

    添加DS18B20工程

---
+++ trunk/C&C++/USB2OW/USB2XXXOWDS18B20/source/USB2XXXOWDS18B20.cpp
@@ -0,0 +1,78 @@
+  /*
+  ******************************************************************************
+  * @file     : USB2XXXOWDS18B20.cpp
+  * @Copyright: usbxyz 
+  * @Revision : ver 1.0
+  * @Date     : 2017/05/27 17:15
+  * @brief    : DS18B20 test demo
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright 2009-2014, usbxyz.com
+  * http://www.usbxyz.com/
+  * All Rights Reserved
+  * 
+  ******************************************************************************
+  */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>  
+#include "usb_device.h"
+#include "usb2ow.h"
+
+//主函数
+int main(int argc, const char* argv[])
+{
+    int DevHandle[10];
+    uint8_t WriteDataBuffer[64];
+    uint8_t ReadDataBuffer[64];
+    DEVICE_INFO DevInfo;
+    bool state;
+    int ret;
+    //扫描查找设备
+    ret = USB_ScanDevice(DevHandle);
+    if(ret <= 0){
+        printf("No device connected!\n");
+        return 0;
+    }
+    //打开设备
+    state = USB_OpenDevice(DevHandle[0]);
+    if(!state){
+        printf("Open device error!\n");
+        return 0;
+    }
+    //获取固件信息
+    char FuncStr[128]={0};
+    state = DEV_GetDeviceInfo(DevHandle[0],&DevInfo,FuncStr);
+    if(!state){
+        printf("Get device infomation error!\n");
+        return 0;
+    }else{
+        printf("Firmware Name:%s\n",DevInfo.FirmwareName);
+        printf("Firmware Build Date:%s\n",DevInfo.BuildDate);
+        printf("Firmware Version:v%d.%d.%d\n",(DevInfo.FirmwareVersion>>24)&0xFF,(DevInfo.FirmwareVersion>>16)&0xFF,DevInfo.FirmwareVersion&0xFFFF);
+        printf("Hardware Version:v%d.%d.%d\n",(DevInfo.HardwareVersion>>24)&0xFF,(DevInfo.HardwareVersion>>16)&0xFF,DevInfo.HardwareVersion&0xFFFF);
+        printf("Firmware Functions:%s\n",FuncStr);
+    }
+
+    OW_Init(DevHandle[0],OW_CH0,500);
+    while(1)
+    {
+        WriteDataBuffer[0] = 0xCC;
+        WriteDataBuffer[1] = 0x44;
+        OW_WriteBytes(DevHandle[0],OW_CH0,WriteDataBuffer,2);
+        Sleep(200);
+        WriteDataBuffer[0] = 0xCC;
+        WriteDataBuffer[1] = 0xBE;
+        OW_WriteReadBytes(DevHandle[0],OW_CH0,WriteDataBuffer,2,ReadDataBuffer,2);
+        int16_t tmp = (ReadDataBuffer[1]<<8)|ReadDataBuffer[0];
+        float temperature = (float)tmp * 0.0625;  
+        printf("t = %04X\n",tmp);
+        printf("temperature = %0.3f\n",temperature);
+        Sleep(1000);
+    }
+
+    return 0;
+}
---
+++ trunk/C&C++/USB2OW/USB2XXXOWDS18B20/source/usb2ow.h
@@ -0,0 +1,70 @@
+/**
+  ******************************************************************************
+  * @file    usb2ow.h
+  * $Author: wdluo $
+  * $Revision: 447 $
+  * $Date:: 2013-06-29 18:24:57 +0800 #$
+  * @brief   usb2ow相关函数和数据类型定义.
+  ******************************************************************************
+  * @attention
+  *
+  *<center><a href="http:\\www.usbxyz.com">http://www.usbxyz.com</a></center>
+  *<center>All Rights Reserved</center></h3>
+  * 
+  ******************************************************************************
+  */
+#ifndef __USB2OW_H_
+#define __USB2OW_H_
+
+#include <stdint.h>
+#ifndef OS_UNIX
+#include <Windows.h>
+#else
+#include <unistd.h>
+#ifndef WINAPI
+#define WINAPI
+#endif
+#endif
+
+
+//定义函数返回错误代码
+#define OW_SUCCESS             (0)   //函数执行成功
+#define OW_ERR_NOT_SUPPORT     (-1)  //适配器不支持该函数
+#define OW_ERR_USB_WRITE_FAIL  (-2)  //USB写数据失败
+#define OW_ERR_USB_READ_FAIL   (-3)  //USB读数据失败
+#define OW_ERR_CMD_FAIL        (-4)  //命令执行失败
+#define OW_ERR_NO_ACK          (-5)  //无从设备应答
+
+//通道定义
+#define OW_CH0             (0x01<<0)
+#define OW_CH1             (0x01<<1)
+#define OW_CH2             (0x01<<2)
+#define OW_CH3             (0x01<<3)
+#define OW_CH4             (0x01<<4)
+#define OW_CH5             (0x01<<5)
+#define OW_CH6             (0x01<<6)
+#define OW_CH7             (0x01<<7)
+#define OW_CH8             (0x01<<8)
+#define OW_CH9             (0x01<<9)
+#define OW_CH10             (0x01<<10)
+#define OW_CH11             (0x01<<11)
+#define OW_CH12             (0x01<<12)
+#define OW_CH13            (0x01<<13)
+#define OW_CH14             (0x01<<14)
+#define OW_CH15             (0x01<<15)
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int WINAPI OW_Init(int DevHandle, unsigned int ChannelMask, unsigned int ResetTimeUs);
+int WINAPI OW_WriteBytes(int DevHandle, unsigned int ChannelMask,unsigned char *pWriteData,unsigned char WriteDataLen);
+int WINAPI OW_ReadBytes(int DevHandle, unsigned int ChannelMask,unsigned char *pReadData,unsigned char ReadDataLen);
+int WINAPI OW_WriteReadBytes(int DevHandle, unsigned int ChannelMask,unsigned char *pWriteData,unsigned char WriteDataLen,unsigned char *pReadData,unsigned char ReadDataLen);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
