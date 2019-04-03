0fc3d96d14f22ae6c39018b5b7e4ac2205346b36
usbxyz <wdluo@embed-net.com>
2019-04-03 17:15:07 +0800

    添加USB2LIN_EX接口测试工程

---
+++ trunk/vb.net/USB2LIN/USB2XXX_LINEX_Test/USB2XXX_LIN_Test/USB2XXX_LIN_Test/USB2XXX_LIN_Test.vb
@@ -0,0 +1,151 @@
+﻿
+#Const LIN_MASTER = False
+
+Module USB2XXX_LIN_Test
+    '若运行程序提示找不到USB2XXX.dll文件，请将USB2XXX.dll文件和libusb-1.0.dll文件拷贝到exe程序输出目录下，比如bin/Debug目录下
+    Sub Main()
+        Dim ret As Int32
+        Dim State As Boolean
+        Dim DeviceHandle(20) As UInt32
+        Dim LINIndex As Byte
+        LINIndex = 0
+        ret = USB_ScanDevice(DeviceHandle)
+        If ret <= 0 Then
+            Console.WriteLine("No device connected")
+        End If
+
+        State = USB_OpenDevice(DeviceHandle(0))
+        If State Then
+            Console.WriteLine("Open device success!")
+        Else
+            Console.WriteLine("Open device failed!")
+            Return
+        End If
+        '初始化配置LIN
+#If LIN_MASTER Then
+        ret = LIN_EX_Init(DeviceHandle(0), LINIndex, 19200, LIN_EX_MASTER)
+#Else
+        ret = LIN_EX_Init(DeviceHandle(0), LINIndex, 19200, LIN_EX_SLAVE)
+#End If
+        If ret <> LIN_EX_SUCCESS Then
+            Console.WriteLine("Config LIN failed!")
+            Return
+        Else
+            Console.WriteLine("Config LIN success!")
+        End If
+
+#If LIN_MASTER Then
+        '主机写数据
+        Dim ID As Byte
+        For ID = 0 To 9
+            Dim msg_len As Byte = 1
+            Dim msg_in(msg_len - 1) As LIN_EX_MSG
+            Dim msg_out(msg_len - 1) As LIN_EX_MSG
+            Dim i As Byte
+            ReDim msg_in(0)
+            ReDim msg_in(0).Data(8)
+            ReDim msg_out(0)
+            ReDim msg_out(0).Data(8)
+            For i = 0 To 7
+                msg_in(0).Data(i) = ID + i
+            Next
+            msg_in(0).DataLen = 8
+            msg_in(0).PID = ID
+            msg_in(0).CheckType = LIN_EX_CHECK_EXT
+            msg_in(0).MsgType = LIN_EX_MSG_TYPE_MW
+            ret = LIN_EX_MasterSync(DeviceHandle(0), LINIndex, msg_in, msg_out, msg_len)
+            If ret <> msg_len Then
+                Console.WriteLine("LIN write data failed!")
+                Return
+            Else
+                Console.Write("Master LIN Write: PID[ID] = 0x{0:X2}[0x{1:X2}] Data = ", msg_out(0).PID, msg_out(0).PID And &H3F)
+                If msg_out(0).DataLen > 0 Then
+                    For i = 0 To msg_out(0).DataLen - 1
+                        Console.Write("0x{0:X2} ", msg_out(0).Data(i))
+                    Next i
+                End If
+                Console.WriteLine("")
+            End If
+            '延时
+            Threading.Thread.Sleep(10)
+        Next ID
+        '主机读数据
+        For ID = 10 To 15
+            Dim msg_len As Byte = 1
+            Dim msg_in(msg_len - 1) As LIN_EX_MSG
+            Dim msg_out(msg_len - 1) As LIN_EX_MSG
+            Dim i As Byte
+            ReDim msg_in(0)
+            ReDim msg_in(0).Data(8)
+            ReDim msg_out(0)
+            ReDim msg_out(0).Data(8)
+            msg_in(0).PID = ID
+            msg_in(0).MsgType = LIN_EX_MSG_TYPE_MW
+            ReDim msg_in(0).Data(8)
+            ret = LIN_EX_MasterSync(DeviceHandle(0), LINIndex, msg_in, msg_out, msg_len)
+            If ret <> msg_len Then
+                Console.WriteLine("LIN read data failed!")
+                Return
+            Else
+                Console.Write("Master LIN Read : PID[ID] = 0x{0:X2}[0x{1:X2}] Data = ", msg_out(0).PID, msg_out(0).PID And &H3F)
+                If msg_out(0).DataLen > 0 Then
+                    For i = 0 To msg_out(0).DataLen - 1
+                        Console.Write("0x{0:X2} ", msg_out(0).Data(i))
+                    Next i
+                End If
+                Console.WriteLine("")
+            End If
+            '延时
+            Threading.Thread.Sleep(10)
+        Next ID
+#Else
+        '设置从机模式下所有ID都为从接收数据模式，这样就可以获取到主机发送过来的所有数据,初始化配置为从机后，默认所有ID都为接收数据模式，所以若是监听LIN总线数据，这个函数可以不用调用
+        Dim LINSlaveData(64) As LIN_EX_MSG
+        Dim i As Int32
+        For i = 0 To 63
+            ReDim LINSlaveData(i)
+            LINSlaveData(i).PID = i      'ID值
+            LINSlaveData(i).MsgType = LIN_EX_MSG_TYPE_SR 'LIN_EX_MSG_TYPE_SR：从机接收数据模式，配置为从机后所有ID默认为该模式
+            ReDim LINSlaveData(i).Data(8)
+        Next i
+        ret = LIN_EX_SlaveSetIDMode(DeviceHandle(0), LINIndex, LINSlaveData, 64)
+        If ret <> LIN_EX_SUCCESS Then
+            Console.WriteLine("Set LIN operation mode failed!")
+            Return
+        Else
+            Console.WriteLine("Set LIN operation mode success!")
+        End If
+        '从机接收数据,若要连续不断的监控LIN总线数据，可以循环调用LIN_SlaveGetData函数
+        Dim LINSlaveDataBuffer(1024) As LIN_EX_MSG '为了防止缓冲区溢出，可以将接收数据缓冲区设置大一点
+        For i = 0 To LINSlaveDataBuffer.Length - 1
+            ReDim LINSlaveDataBuffer(i)
+            ReDim LINSlaveDataBuffer(i).Data(8)
+        Next
+        Console.WriteLine("LIN slave start get data...")
+        While True
+            ret = LIN_EX_SlaveGetData(DeviceHandle(0), LINIndex, LINSlaveDataBuffer)
+            If ret < LIN_EX_SUCCESS Then
+                Console.WriteLine("LIN slave read data error!")
+                Return
+            ElseIf ret > 0 Then
+                For i = 0 To ret - 1
+                    Console.Write("Slave LIN Read : PID[ID] = 0x{0:X2}[0x{1:X2}] Data = ", LINSlaveDataBuffer(i).PID, LINSlaveDataBuffer(i).PID And &H3F)
+                    If LINSlaveDataBuffer(i).DataLen > 0 Then
+                        Dim j As Int32
+                        For j = 0 To LINSlaveDataBuffer(i).DataLen - 1
+                            Console.Write("0x{0:X2} ", LINSlaveDataBuffer(i).Data(j))
+                        Next j
+                    End If
+                    Console.WriteLine("")
+                Next i
+            Else
+                'Console.WriteLine("LIN slave read no data!")
+                '延时
+                Threading.Thread.Sleep(100)
+            End If
+        End While
+#End If
+
+    End Sub
+
+End Module
---
+++ trunk/vb.net/USB2LIN/USB2XXX_LINEX_Test/USB2XXX_LIN_Test/USB2XXX_LIN_Test/usb2lin_ex.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Runtime.InteropServices
+'注意：使用这些函数需要1.5.30及以上的固件才支持
+Module usb2lin_ex
+    'LIN数据收发帧格式定义
+    Public Structure LIN_EX_MSG
+        Dim Timestamp As UInt32 '接收数据时为时间戳，发送数据时为延时的时间，单位为毫秒
+        Dim MsgType As Byte     '帧类型，参考帧类型常数定义
+		Dim CheckType As Byte	'帧数据校验类型
+		Dim DataLen  As Byte	'LIN数据段有效数据字节数
+		Dim Sync  As Byte		'固定值0x55,可以不用赋值，底层会自动处理
+		Dim PID  As Byte		'带校验位的帧ID
+        <MarshalAs(UnmanagedType.ByValArray, SizeConst:=8)> _
+        Dim Data As Byte()      '数据存储区
+		Dim Check  As Byte		'数据和校验，若校验模式是增强型，则计算的数据包含PID，若校验模式为标准型，则计算校验和的数据不包含PID
+		Dim Reserve0  As Byte	'保留数据
+		Dim Reserve1  As Byte	'保留数据
+	End Structure
+    '定义函数返回错误代码
+    Public Const LIN_EX_SUCCESS As Int32 = (0)             '函数执行成功
+    Public Const LIN_EX_ERR_NOT_SUPPORT As Int32 = (-1)    '适配器不支持该函数
+    Public Const LIN_EX_ERR_USB_WRITE_FAIL As Int32 = (-2) 'USB写数据失败
+    Public Const LIN_EX_ERR_USB_READ_FAIL As Int32 = (-3)  'USB读数据失败
+    Public Const LIN_EX_ERR_CMD_FAIL As Int32 = (-4)       '命令执行失败
+    Public Const LIN_EX_ERR_CH_NO_INIT As Int32 = (-5)     '该通道未初始化
+    Public Const LIN_EX_ERR_READ_DATA As Int32 = (-6)      'LIN读数据失败
+	Public Const LIN_EX_ERR_PARAMETER As Int32 = (-7)	   '函数参数传入有误
+    'LIN和校验模式
+    Public Const LIN_EX_CHECK_STD As Byte = 0	'标准校验，不含PID
+    Public Const LIN_EX_CHECK_EXT As Byte = 1	'增强校验，包含PID
+    Public Const LIN_EX_CHECK_USER As Byte = 2	'自定义校验类型，需要用户自己计算并传入Check，不进行自动校验
+    Public Const LIN_EX_CHECK_NONE As Byte = 3	'不计算校验数据
+    Public Const LIN_EX_CHECK_ERROR As Byte = 4	'接收数据校验错误
+    '定义主从模式
+    Public Const LIN_EX_MASTER As Byte = 1
+    Public Const LIN_EX_SLAVE As Byte = 0
+    '帧类型定义
+	Public Const LIN_EX_MSG_TYPE_UN		=0  '未知类型
+	Public Const LIN_EX_MSG_TYPE_MW		=1	'主机向从机发送数据
+	Public Const LIN_EX_MSG_TYPE_MR		=2	'主机从从机读取数据
+	Public Const LIN_EX_MSG_TYPE_SW		=3	'从机发送数据
+	Public Const LIN_EX_MSG_TYPE_SR		=4	'从机接收数据
+	Public Const LIN_EX_MSG_TYPE_BK		=5	'只发送BREAK信号，若是反馈回来的数据，表明只检测到BREAK信号
+	Public Const LIN_EX_MSG_TYPE_SY		=6	'表明检测到了BREAK，SYNC信号
+	Public Const LIN_EX_MSG_TYPE_ID		=7	'表明检测到了BREAK，SYNC，PID信号
+	Public Const LIN_EX_MSG_TYPE_DT		=8	'表明检测到了BREAK，SYNC，PID,DATA信号
+	Public Const LIN_EX_MSG_TYPE_CK		=9	'表明检测到了BREAK，SYNC，PID,DATA,CHECK信号
+
+    Declare Function LIN_EX_Init Lib "USB2XXX.dll" (ByVal DevHandle As UInt32, ByVal LINIndex As Byte, ByVal BaudRate As UInt32, ByVal MasterMode As Byte) As Int32
+    Declare Function LIN_EX_MasterSync Lib "USB2XXX.dll" (ByVal DevHandle As UInt32, ByVal LINIndex As Byte, <[In]()> ByVal pInMsg As LIN_EX_MSG(), <Out()> ByVal pOutMsg As LIN_EX_MSG(), ByVal MsgLen As UInt32) As Int32
+    Declare Function LIN_EX_SlaveSetIDMode Lib "USB2XXX.dll" (ByVal DevHandle As UInt32, ByVal LINIndex As Byte, <[In]()> ByVal pLINMsg As LIN_EX_MSG(), ByVal MsgLen As UInt32) As Int32
+    Declare Function LIN_EX_SlaveGetData Lib "USB2XXX.dll" (ByVal DevHandle As UInt32, ByVal LINIndex As Byte, <Out()> ByVal pLINMsg As LIN_EX_MSG()) As Int32
+    Declare Function LIN_EX_CtrlPowerOut Lib "USB2XXX.dll" (ByVal DevHandle As UInt32, ByVal State As Byte) As Int32
+    Declare Function LIN_EX_GetVbatValue Lib "USB2XXX.dll" (ByVal DevHandle As UInt32, ByVal pBatValue As Int16) As Int32
+    Declare Function LIN_EX_MasterStartSch Lib "USB2XXX.dll" (ByVal DevHandle As UInt32, ByVal LINIndex As Byte, <[In]()> ByVal pLINMsg As LIN_EX_MSG(), ByVal MsgLen As UInt32) As Int32
+    Declare Function LIN_EX_MasterStopSch Lib "USB2XXX.dll" (ByVal DevHandle As UInt32, ByVal LINIndex As Byte) As Int32
+    Declare Function LIN_EX_MasterGetSch Lib "USB2XXX.dll" (ByVal DevHandle As UInt32, ByVal LINIndex As Byte, <Out()> ByVal pLINMsg As LIN_EX_MSG()) As Int32
+End Module
