ba2c86d41da7ef18158b78121d395c4f45ca9f88
usbxyz <wdluo@embed-net.com>
2019-11-15 12:30:09 +0800

    添加USB2LIN C#版本多线程操作范例程序

---
+++ examples/CSharp/USB2LIN/USB2XXX_LIN_EX_TestThread/USB2XXX_LIN_Test/USB2XXX_LIN_Test/Program.cs
@@ -0,0 +1,272 @@
+﻿
+#define LIN_MASTER_TEST
+//#define LIN_SLAVE_TEST
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using USB2XXX;
+/**
+ * 注意：若运行程序提示找不到USB2XXX.dll文件，可以将工程目录下的USB2XXX.dll和libusb-1.0.dll文件拷贝到exe程序输出目录下即可（一般是./bin/Release或者./bin/Debug目录）
+ */
+namespace USB2XXX_LIN_Test
+{
+    class Program
+    {
+        public static bool[] LIN_SendMsgFlag = new bool[2]{false,false};
+        public static String[] MSGTypeStr = new String[10] { "UN", "MW", "MR", "SW", "SR", "BK", "SY", "ID", "DT", "CK" };
+        public static String[] CKTypeStr = new String[5] { "STD", "EXT", "USER", "NONE", "ERROR" };
+        public static void LIN_SendMsgThread(int DevHandle,byte Channel)
+        {
+            while (LIN_SendMsgFlag[Channel])
+            {
+                /************************************主机写数据************************************/
+                int MsgIndex = 0;
+                Byte[] DataBuffer;
+                USB2LIN_EX.LIN_EX_MSG[] LINMsg = new USB2LIN_EX.LIN_EX_MSG[5];
+                USB2LIN_EX.LIN_EX_MSG[] LINOutMsg = new USB2LIN_EX.LIN_EX_MSG[10];
+                //添加第一帧数据
+                LINMsg[MsgIndex] = new USB2LIN_EX.LIN_EX_MSG();
+                LINMsg[MsgIndex].MsgType = USB2LIN_EX.LIN_EX_MSG_TYPE_BK;//只发送BREAK信号，一般用于唤醒休眠中的从设备
+                LINMsg[MsgIndex].Timestamp = 10;//发送该帧数据之后的延时时间，最小建议设置为1
+                MsgIndex++;
+                //添加第二帧数据
+                LINMsg[MsgIndex] = new USB2LIN_EX.LIN_EX_MSG();
+                LINMsg[MsgIndex].MsgType = USB2LIN_EX.LIN_EX_MSG_TYPE_MW;//主机发送数据
+                LINMsg[MsgIndex].DataLen = 8;//实际要发送的数据字节数
+                LINMsg[MsgIndex].Timestamp = 10;//发送该帧数据之后的延时时间
+                LINMsg[MsgIndex].CheckType = USB2LIN_EX.LIN_EX_CHECK_EXT;//增强校验
+                LINMsg[MsgIndex].PID = 0x32;//可以只传入ID，校验位底层会自动计算
+                LINMsg[MsgIndex].Data = new Byte[8];//必须分配8字节空间
+                DataBuffer = new Byte[8] { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };//根据自己实际情况修改数据
+                for (int i = 0; i < LINMsg[MsgIndex].DataLen; i++)//循环填充8字节数据
+                {
+                    LINMsg[MsgIndex].Data[i] = DataBuffer[i];
+                }
+                MsgIndex++;
+                //添加第三帧数据
+                LINMsg[MsgIndex] = new USB2LIN_EX.LIN_EX_MSG();
+                LINMsg[MsgIndex].MsgType = USB2LIN_EX.LIN_EX_MSG_TYPE_MW;//主机发送数据
+                LINMsg[MsgIndex].DataLen = 8;//实际要发送的数据字节数
+                LINMsg[MsgIndex].Timestamp = 10;//发送该帧数据之后的延时时间
+                LINMsg[MsgIndex].CheckType = USB2LIN_EX.LIN_EX_CHECK_EXT;//增强校验
+                LINMsg[MsgIndex].PID = 0x32;//可以只传入ID，校验位底层会自动计算
+                LINMsg[MsgIndex].Data = new Byte[8];//必须分配8字节空间
+                DataBuffer = new Byte[8] { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };//根据自己实际情况修改数据
+                for (int i = 0; i < LINMsg[MsgIndex].DataLen; i++)//循环填充8字节数据
+                {
+                    LINMsg[MsgIndex].Data[i] = DataBuffer[i];
+                }
+                MsgIndex++;
+                /********************需要发送更多帧数据，请按照前面的方式继续添加********************/
+                //将数组转换成指针
+                IntPtr pt = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(USB2LIN_EX.LIN_EX_MSG)) * MsgIndex);
+                int ret = USB2LIN_EX.LIN_EX_MasterSync(DevHandle, Channel, LINMsg, pt, MsgIndex);
+                if (ret < USB2LIN_EX.LIN_EX_SUCCESS)
+                {
+                    Console.WriteLine("MasterSync LIN failed!");
+                    //释放内存
+                    Marshal.FreeHGlobal(pt);
+                    return;
+                }
+                else
+                {
+                    //主机发送数据成功后，也会接收到发送出去的数据，通过接收回来的数据跟发送出去的数据对比，可以判断发送数据的时候，数据是否被冲突
+                    Console.WriteLine("MsgLen = {0}", ret);
+                    for (int i = 0; i < ret; i++)
+                    {
+                        LINOutMsg[i] = (USB2LIN_EX.LIN_EX_MSG)Marshal.PtrToStructure((IntPtr)((UInt32)pt + i * Marshal.SizeOf(typeof(USB2LIN_EX.LIN_EX_MSG))), typeof(USB2LIN_EX.LIN_EX_MSG));
+                        Console.Write("{0} SYNC[{1:X2}] PID[{2:X2}] ", MSGTypeStr[LINOutMsg[i].MsgType], LINOutMsg[i].Sync, LINOutMsg[i].PID);
+                        for (int j = 0; j < LINOutMsg[i].DataLen; j++)
+                        {
+                            Console.Write("{0:X2} ", LINOutMsg[i].Data[j]);
+                        }
+                        Console.WriteLine("[{0}][{1:X2}] [{2}:{3}:{4}.{5}]", CKTypeStr[LINOutMsg[i].CheckType], LINOutMsg[i].Check, (LINOutMsg[i].Timestamp / 36000000) % 60, (LINOutMsg[i].Timestamp / 600000) % 60, (LINOutMsg[i].Timestamp / 10000) % 60, (LINOutMsg[i].Timestamp / 10) % 1000);
+                    }
+                }
+                //释放内存
+                Marshal.FreeHGlobal(pt);
+                Thread.Sleep(1000);
+            }
+        }
+        static void Main(string[] args)
+        {
+            usb_device.DEVICE_INFO DevInfo = new usb_device.DEVICE_INFO();
+            Int32[] DevHandles = new Int32[20];
+            Int32 DevHandle = 0;
+            bool state;
+            Int32 DevNum, ret=0;
+            //扫描查找设备
+            DevNum = usb_device.USB_ScanDevice(DevHandles);
+            if (DevNum <= 0)
+            {
+                Console.WriteLine("No device connected!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Have {0} device connected!", DevNum);
+            }
+            DevHandle = DevHandles[0];
+            //打开设备
+            state = usb_device.USB_OpenDevice(DevHandle);
+            if (!state)
+            {
+                Console.WriteLine("Open device error!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Open device success!");
+            }
+            //获取固件信息
+            StringBuilder FuncStr = new StringBuilder(256);
+            state = usb_device.DEV_GetDeviceInfo(DevHandle, ref DevInfo, FuncStr);
+            if (!state)
+            {
+                Console.WriteLine("Get device infomation error!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Firmware Info:");
+                Console.WriteLine("    Name:" + Encoding.Default.GetString(DevInfo.FirmwareName));
+                Console.WriteLine("    Build Date:" + Encoding.Default.GetString(DevInfo.BuildDate));
+                Console.WriteLine("    Firmware Version:v{0}.{1}.{2}", (DevInfo.FirmwareVersion >> 24) & 0xFF, (DevInfo.FirmwareVersion >> 16) & 0xFF, DevInfo.FirmwareVersion & 0xFFFF);
+                Console.WriteLine("    Hardware Version:v{0}.{1}.{2}", (DevInfo.HardwareVersion >> 24) & 0xFF, (DevInfo.HardwareVersion >> 16) & 0xFF, DevInfo.HardwareVersion & 0xFFFF);
+                Console.WriteLine("    Functions:" + DevInfo.Functions.ToString("X8"));
+                Console.WriteLine("    Functions String:" + FuncStr);
+            }
+            //初始化配置LIN
+#if LIN_MASTER_TEST
+            ret = USB2LIN_EX.LIN_EX_Init(DevHandle, 0, 19200, 1);
+#else
+            ret = USB2LIN_EX.LIN_EX_Init(DevHandle, 0, 19200, 0);
+#endif
+            if (ret != USB2LIN_EX.LIN_EX_SUCCESS)
+            {
+                Console.WriteLine("Config LIN failed!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Config LIN Success!");
+            }
+#if LIN_MASTER_TEST
+            ret = USB2LIN_EX.LIN_EX_Init(DevHandle, 1, 19200, 1);
+#else
+            ret = USB2LIN_EX.LIN_EX_Init(DevHandle, 1, 19200, 0);
+#endif
+            if (ret != USB2LIN_EX.LIN_EX_SUCCESS)
+            {
+                Console.WriteLine("Config LIN failed!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Config LIN Success!");
+            }
+#if LIN_MASTER_TEST
+            //启动多线程写数据
+            Thread LIN1SendMsgThread = new Thread(() => LIN_SendMsgThread(DevHandle, 0));
+            Thread LIN2SendMsgThread = new Thread(() => LIN_SendMsgThread(DevHandle, 1));
+            LIN_SendMsgFlag[0] = true;
+            LIN_SendMsgFlag[1] = true;
+            LIN1SendMsgThread.Start();
+            LIN2SendMsgThread.Start();
+            //按下回车后结束发送接收线程
+            Console.ReadLine();
+            LIN_SendMsgFlag[0] = false;
+            LIN_SendMsgFlag[1] = false;
+            LIN1SendMsgThread.Join();
+            LIN2SendMsgThread.Join();
+#else
+            /************************************从机发送数据************************************/
+            int MsgIndex = 0;
+            Byte[] DataBuffer;
+            //设置ID为LIN_EX_MSG_TYPE_SW模式，从机接收到主机发送的帧头后就会返回预先定义好的数据
+            USB2LIN_EX.LIN_EX_MSG[] LINSlaveMsg = new USB2LIN_EX.LIN_EX_MSG[3];
+            //配置第一帧数据
+            LINSlaveMsg[MsgIndex] = new USB2LIN_EX.LIN_EX_MSG();
+            LINSlaveMsg[MsgIndex].MsgType = USB2LIN_EX.LIN_EX_MSG_TYPE_SW;//从机发送数据模式
+            LINSlaveMsg[MsgIndex].CheckType = USB2LIN_EX.LIN_EX_CHECK_EXT;//配置为增强校验
+            LINSlaveMsg[MsgIndex].PID = 0x34;//可以只传入ID，校验位底层会自动计算
+            LINSlaveMsg[MsgIndex].Data = new Byte[8];//必须分配8字节空间
+            LINSlaveMsg[MsgIndex].DataLen = 8;//实际要发送的数据字节数
+            DataBuffer = new Byte[8] { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };//从机返回的数据，根据自己实际情况修改数据
+            for (int i = 0; i < LINSlaveMsg[MsgIndex].DataLen; i++)//循环填充8字节数据
+            {
+                LINSlaveMsg[MsgIndex].Data[i] = DataBuffer[i];
+            }
+            MsgIndex++;
+            //配置第二帧数据
+            LINSlaveMsg[MsgIndex] = new USB2LIN_EX.LIN_EX_MSG();
+            LINSlaveMsg[MsgIndex].MsgType = USB2LIN_EX.LIN_EX_MSG_TYPE_SW;//从机发送数据模式
+            LINSlaveMsg[MsgIndex].CheckType = USB2LIN_EX.LIN_EX_CHECK_EXT;//配置为增强校验
+            LINSlaveMsg[MsgIndex].PID = 0x35;//可以只传入ID，校验位底层会自动计算
+            LINSlaveMsg[MsgIndex].Data = new Byte[8];//必须分配8字节空间
+            LINSlaveMsg[MsgIndex].DataLen = 8;//实际要发送的数据字节数
+            DataBuffer = new Byte[8] { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };//从机返回的数据，根据自己实际情况修改数据
+            for (int i = 0; i < LINSlaveMsg[MsgIndex].DataLen; i++)//循环填充8字节数据
+            {
+                LINSlaveMsg[MsgIndex].Data[i] = DataBuffer[i];
+            }
+            MsgIndex++;
+            //配置第三帧数据
+            LINSlaveMsg[MsgIndex] = new USB2LIN_EX.LIN_EX_MSG();
+            LINSlaveMsg[MsgIndex].MsgType = USB2LIN_EX.LIN_EX_MSG_TYPE_SW;//从机发送数据模式
+            LINSlaveMsg[MsgIndex].CheckType = USB2LIN_EX.LIN_EX_CHECK_EXT;//配置为增强校验
+            LINSlaveMsg[MsgIndex].PID = 0x36;//可以只传入ID，校验位底层会自动计算
+            LINSlaveMsg[MsgIndex].Data = new Byte[8];//必须分配8字节空间
+            LINSlaveMsg[MsgIndex].DataLen = 8;//实际要发送的数据字节数
+            DataBuffer = new Byte[8] { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };//从机返回的数据，根据自己实际情况修改数据
+            for (int i = 0; i < LINSlaveMsg[MsgIndex].DataLen; i++)//循环填充8字节数据
+            {
+                LINSlaveMsg[MsgIndex].Data[i] = DataBuffer[i];
+            }
+            MsgIndex++;
+            /********************需要配置更多帧，请按照前面的方式继续添加********************/
+            //默认所有ID都是从机接收数据模式，该模式下可以用于数据的监听
+            //调用该函数后，只会修改传入对应ID的模式，其他的不会被改变
+            ret = USB2LIN_EX.LIN_EX_SlaveSetIDMode(DevHandle, LINIndex, LINSlaveMsg, MsgIndex);
+            if(ret != USB2LIN_EX.LIN_EX_SUCCESS){
+                Console.WriteLine("Config LIN ID Mode failed!");
+                return;
+            }else{
+                Console.WriteLine("Config LIN ID Mode Success!");
+            }
+            //循环获取接收到的数据，该操作可以用作LIN总线数据监控
+            Console.WriteLine("Start Get LIN Data...");
+            int RunTimeMs = 10000;//只运行10秒钟，10秒钟后结束数据监听，可自行修改
+            while (RunTimeMs > 0)
+            {
+                USB2LIN_EX.LIN_EX_MSG[] LINMsg = new USB2LIN_EX.LIN_EX_MSG[1024];//缓冲区尽量大一点，防止益处
+                //将数组转换成指针
+                IntPtr pt = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(USB2LIN_EX.LIN_EX_MSG)) * LINMsg.Length);
+
+                ret = USB2LIN_EX.LIN_EX_SlaveGetData(DevHandle, LINIndex, pt);
+                for(int i=0;i<ret;i++){
+                    LINMsg[i] = (USB2LIN_EX.LIN_EX_MSG)Marshal.PtrToStructure((IntPtr)((UInt32)pt + i * Marshal.SizeOf(typeof(USB2LIN_EX.LIN_EX_MSG))), typeof(USB2LIN_EX.LIN_EX_MSG));
+                    Console.WriteLine("{0} SYNC[{1:X2}] PID[{2:X2}] ", MSGTypeStr[LINMsg[i].MsgType], LINMsg[i].Sync, LINMsg[i].PID);
+                    for(int j=0;j<LINMsg[i].DataLen;j++){
+                        Console.Write("{0:X2} ", LINMsg[i].Data[j]);
+                    }
+                    Console.WriteLine("[{0}][{1:X2}] [{2}]:{3}]:{4}].{5}]]\n", CKTypeStr[LINMsg[i].CheckType], LINMsg[i].Check, (LINMsg[i].Timestamp / 36000000) % 60, (LINMsg[i].Timestamp / 600000) % 60, (LINMsg[i].Timestamp / 10000) % 60, (LINMsg[i].Timestamp / 10) % 1000);
+                }
+                //释放内存
+                Marshal.FreeHGlobal(pt);
+                System.Threading.Thread.Sleep(10);
+                RunTimeMs -= 10;
+            }
+#endif
+            Console.WriteLine("Close Device!");
+            //关闭设备
+            usb_device.USB_CloseDevice(DevHandle);
+            return;
+        }
+    }
+}
---
+++ examples/CSharp/USB2LIN/USB2XXX_LIN_EX_TestThread/USB2XXX_LIN_Test/USB2XXX_LIN_Test/usb2lin_ex.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+//注意：使用这些函数需要1.5.30及以上的固件才支持
+namespace USB2XXX
+{
+    class USB2LIN_EX
+    {
+        //定义函数返回错误代码
+        public const Int32 LIN_EX_SUCCESS            = (0);   //函数执行成功
+        public const Int32 LIN_EX_ERR_NOT_SUPPORT    = (-1);  //适配器不支持该函数
+        public const Int32 LIN_EX_ERR_USB_WRITE_FAIL = (-2);  //USB写数据失败
+        public const Int32 LIN_EX_ERR_USB_READ_FAIL  = (-3);  //USB读数据失败
+        public const Int32 LIN_EX_ERR_CMD_FAIL       = (-4);  //命令执行失败
+        public const Int32 LIN_EX_ERR_CH_NO_INIT     = (-5);  //该通道未初始化
+        public const Int32 LIN_EX_ERR_READ_DATA      = (-6);  //LIN读数据失败
+        //LIN和校验模式
+        public const Byte LIN_EX_CHECK_STD    = 0;	//标准校验，不含PID
+        public const Byte LIN_EX_CHECK_EXT    = 1;	//增强校验，包含PID
+        public const Byte LIN_EX_CHECK_USER   = 2;  //自定义校验类型，需要用户自己计算并传入Check，不进行自动校验
+        public const Byte LIN_EX_CHECK_NONE   = 3;  //接收数据校验错误
+        public const Byte LIN_EX_CHECK_ERROR  = 4;  //接收数据校验错误
+        //定义主从模式
+        public const Byte LIN_EX_MASTER       = 1;//主机
+        public const Byte LIN_EX_SLAVE        = 0;//从机
+
+        public const Byte LIN_EX_MSG_TYPE_UN    =   0;  //未知类型
+        public const Byte LIN_EX_MSG_TYPE_MW    =	1;	//主机向从机发送数据
+        public const Byte LIN_EX_MSG_TYPE_MR	=	2;	//主机从从机读取数据
+        public const Byte LIN_EX_MSG_TYPE_SW	=	3;	//从机发送数据
+        public const Byte LIN_EX_MSG_TYPE_SR	=	4;	//从机接收数据
+        public const Byte LIN_EX_MSG_TYPE_BK	=	5;	//只发送BREAK信号，若是反馈回来的数据，表明只检测到BREAK信号
+        public const Byte LIN_EX_MSG_TYPE_SY	=	6;	//表明检测到了BREAK，SYNC信号
+        public const Byte LIN_EX_MSG_TYPE_ID	=	7;	//表明检测到了BREAK，SYNC，PID信号
+        public const Byte LIN_EX_MSG_TYPE_DT	=	8;	//表明检测到了BREAK，SYNC，PID,DATA信号
+        public const Byte LIN_EX_MSG_TYPE_CK	=	9;	//表明检测到了BREAK，SYNC，PID,DATA,CHECK信号
+
+        //LIN数据帧格式定义
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]  
+        public struct LIN_EX_MSG
+        {
+            [MarshalAs(UnmanagedType.U4)]
+            public UInt32  Timestamp;    //时间戳
+            [MarshalAs(UnmanagedType.U1)]
+            public Byte MsgType;	    //帧类型
+            [MarshalAs(UnmanagedType.U1)]
+            public Byte CheckType;    //校验类型
+            [MarshalAs(UnmanagedType.U1)]
+            public Byte DataLen;	    //LIN数据段有效数据字节数
+            [MarshalAs(UnmanagedType.U1)]
+            public Byte Sync;			//固定值，0x55
+            [MarshalAs(UnmanagedType.U1)]
+            public Byte PID;			//帧ID		
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8, ArraySubType = UnmanagedType.U1)]
+            public Byte[] Data;	    //数据
+            [MarshalAs(UnmanagedType.U1)]
+            public Byte Check;		//校验,只有校验数据类型为LIN_EX_CHECK_USER的时候才需要用户传入数据
+            [MarshalAs(UnmanagedType.U1)]
+            public Byte BreakBits;  //该帧的BRAK信号位数，有效值为10到26，若设置为其他值则默认为13位
+            [MarshalAs(UnmanagedType.U1)]
+            public Byte Reserve1;
+        }
+        //USB2LIN相关函数定义
+        [DllImport("USB2XXX.dll")]
+        public static extern Int32  LIN_EX_Init(Int32 DevHandle,Byte LINIndex,Int32 BaudRate,Byte MasterMode);
+        [DllImport("USB2XXX.dll")]
+        public static extern Int32 LIN_EX_MasterSync(Int32 DevHandle, Byte LINIndex, LIN_EX_MSG[] pInMsg, IntPtr pOutMsg, Int32 MsgLen);
+        [DllImport("USB2XXX.dll")]
+        public static extern Int32  LIN_EX_SlaveSetIDMode(Int32 DevHandle,Byte LINIndex,LIN_EX_MSG[] pLINMsg,Int32 MsgLen);
+        [DllImport("USB2XXX.dll")]
+        public static extern Int32  LIN_EX_SlaveGetIDMode(Int32 DevHandle,Byte LINIndex,IntPtr pLINMsg);
+        [DllImport("USB2XXX.dll")]
+        public static extern Int32 LIN_EX_SlaveGetData(Int32 DevHandle, Byte LINIndex, IntPtr pLINMsg);
+        [DllImport("USB2XXX.dll")]
+        public static extern Int32  LIN_EX_CtrlPowerOut(Int32 DevHandle,Byte State);
+        [DllImport("USB2XXX.dll")]
+        public static extern Int32  LIN_EX_GetVbatValue(Int32 DevHandle,Int16[] pBatValue);
+        [DllImport("USB2XXX.dll")]
+        public static extern Int32  LIN_EX_MasterStartSch(Int32 DevHandle,Byte LINIndex,LIN_EX_MSG[] pLINMsg,Int32 MsgLen);
+        [DllImport("USB2XXX.dll")]
+        public static extern Int32  LIN_EX_MasterStopSch(Int32 DevHandle,Byte LINIndex);
+        [DllImport("USB2XXX.dll")]
+        public static extern Int32  LIN_EX_MasterGetSch(Int32 DevHandle,Byte LINIndex,IntPtr pLINMsg);
+        [DllImport("USB2XXX.dll")]
+        public static extern Int32  LIN_EX_MasterOfflineSch(Int32 DevHandle,Byte LINIndex,Int32 BaudRate,LIN_EX_MSG[] pLINMsg,Int32 MsgLen);
+        [DllImport("USB2XXX.dll")]
+        public static extern Int32  LIN_EX_DecodeListFile(string pFileName,Byte CheckType,Int32 BaudRate,Byte[] pReadDataList,Byte ReadDataListLen,Byte[] pCheckTypeList,Byte CheckTypeListLen);
+        [DllImport("USB2XXX.dll")]
+        public static extern Int32  LIN_EX_GetListFileMsg(Int32 MsgIndex,Int32 MsgLen,IntPtr pLINMsg);
+    }
+}
